f2c338353d855e030a8526636635756613fc00f6
phoenixvane <32545460+phoenixvane@users.noreply.github.com>
2020-08-27 04:43:08 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+let bearing = 0
+basic.forever(function () {
+    bearing = input.compassHeading()
+    if (bearing >= 315 || bearing < 45) {
+        basic.showArrow(ArrowNames.North)
+    } else if (bearing >= 45 && bearing < 135) {
+        basic.showArrow(ArrowNames.West)
+    } else if (bearing >= 135 && bearing < 225) {
+        basic.showArrow(ArrowNames.South)
+    } else if (bearing >= 225 && bearing < 315) {
+        basic.showArrow(ArrowNames.East)
+    } else {
+        basic.clearScreen()
+    }
+})
